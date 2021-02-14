76486735d47f817358124344e22bc8378b29b8a7
Harshad Ranganathan <32612787+HarshadRanganathan@users.noreply.github.com>
2021-02-14 10:28:09 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,187 @@
+variable "region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace, which could be your organization name, e.g. 'eg' or 'cp'"
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage, e.g. 'prod', 'staging', 'dev' or 'testing'"
+}
+
+variable "name" {
+  type        = string
+  description = "Solution name, e.g. 'app' or 'cluster'"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit`,`XYZ`)"
+}
+
+variable "update_launch_template_default_version" {
+  type = bool
+  description = "Whether to update Launch Template Default Version on each update."
+  default = false
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type to launch"
+}
+
+variable "kubernetes_version" {
+  type        = string
+  default     = ""
+  description = "Desired Kubernetes master version. If you do not specify a value, the latest available version is used"
+}
+
+variable "health_check_type" {
+  type        = string
+  description = "Controls how health checking is done. Valid values are `EC2` or `ELB`"
+}
+
+variable "public_access_cidrs" {
+  description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint. Defaults to 0.0.0.0/0"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "associate_public_ip_address" {
+  type        = bool
+  description = "Associate a public IP address with an instance in a VPC"
+}
+
+variable "max_size" {
+  type        = number
+  description = "The maximum size of the AutoScaling Group"
+}
+
+variable "min_size" {
+  type        = number
+  description = "The minimum size of the AutoScaling Group"
+}
+
+variable "wait_for_capacity_timeout" {
+  type        = string
+  description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out. Setting this to '0' causes Terraform to skip all Capacity Waiting behavior"
+}
+
+variable "autoscaling_policies_enabled" {
+  type        = bool
+  description = "Whether to create `aws_autoscaling_policy` and `aws_cloudwatch_metric_alarm` resources to control Auto Scaling"
+}
+
+variable "disable_imds_v1" {
+  type        = bool
+  description = "Set to true to require the use of IMDSv2"
+  default     = false
+}
+
+variable "cpu_utilization_high_threshold_percent" {
+  type        = number
+  description = "Worker nodes AutoScaling Group CPU utilization high threshold percent"
+}
+
+variable "cpu_utilization_low_threshold_percent" {
+  type        = number
+  description = "Worker nodes AutoScaling Group CPU utilization low threshold percent"
+}
+
+variable "map_additional_aws_accounts" {
+  description = "Additional AWS account numbers to add to `config-map-aws-auth` ConfigMap"
+  type        = list(string)
+  default     = []
+}
+
+variable "map_additional_iam_roles" {
+  description = "Additional IAM roles to add to `config-map-aws-auth` ConfigMap"
+
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = []
+}
+
+variable "map_additional_iam_users" {
+  description = "Additional IAM users to add to `config-map-aws-auth` ConfigMap"
+
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = []
+}
+
+variable "oidc_provider_enabled" {
+  type        = bool
+  default     = false
+  description = "Create an IAM OIDC identity provider for the cluster, then you can create IAM roles to associate with a service account in the cluster, instead of using kiam or kube2iam. For more information, see https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html"
+}
+
+# Autoscaling group variables
+variable "instance_market_options" {
+  description = "The market (purchasing) option for the instances"
+  /*
+  type = object({
+    market_type = string
+    spot_options = object({
+      block_duration_minutes         = number
+      instance_interruption_behavior = string
+      max_price                      = number
+      spot_instance_type             = string
+      valid_until                    = string
+    })
+  })
+*/
+  default = null
+}
+
+########################
+## EKS Cluster variables
+########################
+
+variable "cluster_endpoint_private_access" {
+  type        = bool
+  default     = false
+  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled. Default to AWS EKS resource and it is false"
+}
+
+variable "cluster_endpoint_public_access" {
+  type        = bool
+  default     = true
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled. Default to AWS EKS resource and it is true"
+}
+
+variable "enabled_cluster_log_types" {
+  type        = list(string)
+  default     = []
+  description = "A list of the desired control plane logging to enable. For more information, see https://docs.aws.amazon.com/en_us/eks/latest/userguide/control-plane-logs.html. Possible values [`api`, `audit`, `authenticator`, `controllerManager`, `scheduler`]"
+}
